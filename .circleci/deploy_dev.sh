09348432bb80a44ce689b476d632e056277fcf0a
Andrey Radionov <andyomsk@gmail.com>
2018-01-23 22:39:47 +0300

    hu

--- .circleci/deploy_dev.sh
+++
@@ -1,6 +0,0 @@
-curl
--F “status=2”
--F “notify=1”
--F “ipa=@APK FULL PATH(ChildEduGame/build/outputs/apk/APK_NAME)”
--H “X-HockeyAppToken: $HOCKEYAPP_TOKEN”
-https://rink.hockeyapp.net/api/2/apps/$AndroidFirstAppId/app_versions/upload7
